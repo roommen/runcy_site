5d16bee0172ac302f4fb199c62bc0c3d4711962b
Runcy Oommen <runcy.oommen@gmail.com>
2019-02-08 23:23:41 +0530

    Update resume.py

--- api/resume.py
+++ api/resume.py
@@ -1,6 +1,6 @@
 def resume():
-            return {"name": "Runcy Oommen", "website": "https://runcy.me", 
-                    "email": "runcy.oommen@gmail.com", "location": "Bangalore, India"}
+            return {"Name": "Runcy Oommen", "URL": "https://runcy.me", 
+                    "Email": "runcy.oommen@gmail.com", "Location": "Bangalore, India"}
 
 def lambda_handler(event, context):
     return resume()
